c73670ba6ae877790b80a8f19ae0aba108184909
tongoonamujera <tongoonamujera@gmail.com>
2021-07-10 11:24:18 +0200

    Add more querruerries

--- zoo.sql
+++ zoo.sql
@@ -1,2 +1,38 @@
 --select basics
+SELECT population FROM world
+  WHERE name = 'Germany'
 
+SELECT name, population FROM world
+  WHERE name IN ('Sweden', 'Norway', 'Denmark');
+
+SELECT name, area FROM world
+  WHERE area BETWEEN 200000 AND 250000
+
+--SELECT names
+--name starting from a character
+SELECT name FROM world
+  WHERE name LIKE 'Y%'
+
+--names ending with a character
+SELECT name FROM world
+  WHERE name LIKE '%y'
+
+--names including a character
+SELECT name FROM world
+  WHERE name LIKE '%x%'
+
+SELECT name FROM world
+  WHERE name LIKE '%land'
+
+SELECT name FROM world
+  WHERE name LIKE 'C%%ia'
+
+SELECT name FROM world
+  WHERE name LIKE '%oo%'
+
+SELECT name FROM world
+  WHERE name LIKE '%a%a%a%'
+
+SELECT name FROM world
+  WHERE name LIKE '_t%'
+ORDER BY name
